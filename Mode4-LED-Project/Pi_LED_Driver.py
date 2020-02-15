01f3963b571d38b7e839d889c8a9eeb19e1a62c4
John Richardson <john.richardson.0617@gmail.com>
2020-02-15 10:36:18 -0500

    Module 4 Code Added Multiple Topics Subscription, Additional cleanup

--- Mode4-LED-Project/Pi_LED_Driver.py
+++ Mode4-LED-Project/Pi_LED_Driver.py
@@ -6,6 +6,9 @@ import MQTTSubscriber as ms
 
 # Connect the blue LED to digital port D5
 BLUE_LED = 5
+RED_LED = 4
+GREEN_LED = 6
+
 # Set the blue LED pin to output mode
 grovepi.pinMode(BLUE_LED, "OUTPUT")
 time.sleep(1)  # give the hardware time to initialize
@@ -20,8 +23,8 @@ def on_connect(client, userdata, flags, rc):
      :param rc: the connection result
      :return: None
      """
-    # subscribe to the LEDs topic when connected
-    topics = [("SNHU/IT697/leds", 2), ("SNHU/IT697/leds/blue", 2)]
+    # subscribe to the LEDs topics when connected
+    topics = [("SNHU/IT697/leds/red", 2), ("SNHU/IT697/leds/green", 2), ("SNHU/IT697/leds/blue", 2)]
     print("Connected to MQTT Broker........")
     # client.subscribe("SNHU/IT697/leds")
     client.subscribe(topics)
@@ -40,19 +43,20 @@ def on_message(client, userdata, msg):
     print(msg.topic, msg.payload)
     _topic = msg.topic
     led_payload = jsonpickle.decode(msg.payload)
-    if _topic == 'SNHU/IT697/leds':
-        print("Processing " + _topic + " with msg " +msg.payload)
-        # payload = json.loads(msg.payload)
-        # the legal values for analogWrite are 0-255
-        grovepi.analogWrite(BLUE_LED, led_payload['blue'])
+    if _topic == 'SNHU/IT697/leds/red':
+        print("Processing " + _topic + " with msg " + msg.payload)
+        # write discrete for RED LED
+        grovepi.analogWrite(RED_LED, led_payload['red'])
+    elif _topic == 'SNHU/IT697/leds/green':
+        print("Processing " + _topic + " with msg " + msg.payload)
+        # write discrete for GREEN LED
+        grovepi.analogWrite(GREEN_LED, led_payload['green'])
     elif _topic == 'SNHU/IT697/leds/blue':
         print("Processing " + _topic + " with msg " + msg.payload)
-        # payload = json.loads(msg.payload)
-        # the legal values for analogWrite are 0-255
+        # write discrete for BLUE LED
         grovepi.analogWrite(BLUE_LED, led_payload['blue'])
     else:
         print("No Registered Topic......")
-        return
 
 
 subscriber = ms.MQTTSubscriber(mqtt_host="localhost", mqtt_client_id="LOCAL_SUBSCRIBER", port=1883)
