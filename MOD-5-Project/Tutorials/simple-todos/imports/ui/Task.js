88923a2dbb537013886e38a60c3f37d954ae542e
johnnyrich0617 <john.richardson.0617@gmail.com>
2020-02-17 15:54:48 -0500

    Base Application tagged insecure

---
+++ MOD-5-Project/Tutorials/simple-todos/imports/ui/Task.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+// Task component - represents a single todo item
+import { Tasks } from '../api/tasks.js';
+
+
+
+// Task component - represents a single todo item
+
+export default class Task extends Component {
+
+    toggleChecked() {
+        // Set the checked property to the opposite of its current value
+        Tasks.update(this.props.task._id, {
+            $set: { checked: !this.props.task.checked },
+        });
+    }
+
+    deleteThisTask() {
+        Tasks.remove(this.props.task._id);
+    }
+
+    render() {
+        // Give tasks a different className when they are checked off,
+        // so that we can style them nicely in CSS
+        const taskClassName = this.props.task.checked ? 'checked' : '';
+
+        return (
+            <li className={taskClassName}>
+                <button className="delete" onClick={this.deleteThisTask.bind(this)}>
+                    &times;
+                </button>
+                <input
+                    type="checkbox"
+                    readOnly
+                    checked={!!this.props.task.checked}
+                    onClick={this.toggleChecked.bind(this)}
+                />
+                <span className="text">
+                    <strong>{this.props.task.username}</strong> : {this.props.task.text}
+                </span>
+            </li>
+        );
+    }
+}
+
