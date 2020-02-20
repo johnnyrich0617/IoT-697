008616b9e86fccd7f61ea0efd440cb4034f9e19b
John Richardson <john.richardson.0617@gmail.com>
2020-02-20 11:32:15 -0500

    Created runtime_env.sh for exporting
    Shell to be sourced

---
+++ Mod2-3-Project/runtime_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Exporting Python Path to runtime for external package locations
+
+#set the project base dir
+projectPackageBase=/home/pi/Projects/IoT-697
+
+#export the runtime python path to be sourced
+export PYTHONPATH=$PYTHONPATH:$projectPackageBase/messages:$projectPackageBase/utils:$projectPackageBase/pi_sensors:$projectPackageBase/pi_sensors/publishers:$projectPackageBase/pi_sensors/subscribers
