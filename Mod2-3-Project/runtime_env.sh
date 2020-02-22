928962dd51a8e6ab85a8fee5ceb3e44501e941d9
John Richardson <john.richardson.0617@gmail.com>
2020-02-22 17:59:20 -0500

    Update runtime_env.sh

--- Mod2-3-Project/runtime_env.sh
+++ Mod2-3-Project/runtime_env.sh
@@ -2,7 +2,10 @@
 #Exporting Python Path to runtime for external package locations
 
 #set the project base dir
-projectPackageBase=/home/pi/Projects/IoT-697
+#projectPackageBase=/home/pi/Projects/IoT-697
 
 #export the runtime python path to be sourced
-export PYTHONPATH=$PYTHONPATH:$projectPackageBase/messages:$projectPackageBase/utils:$projectPackageBase/pi_sensors:$projectPackageBase/pi_sensors/publishers:$projectPackageBase/pi_sensors/subscribers
+#export PYTHONPATH=$PYTHONPATH:$projectPackageBase/messages:$projectPackageBase/utils:$projectPackageBase/pi_sensors:$projectPackageBase/pi_sensors/publishers:$projectPackageBase/pi_sensors/subscribers
+
+
+# Need to change this to copy packages to ./local....
